da42ba7a288418a1abc9a85cfa63113d7ecb94af
Shivraj <54741030+Shivraj0@users.noreply.github.com>
2021-04-24 23:20:32 +0530

    added css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,29 @@ module.exports = {
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
+        },
+        {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: [
+                { 
+                    loader: 'style-loader',
+                    options: {
+                        injectType: 'singletonStyleTag' // Default is `styleTag` which injects through multiple <style></style> tags.
+                    }
+                },
+                { loader: 'css-loader' },
+                { 
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            plugins: [
+                                [ 'autoprefixer', {}, ],
+                            ],
+                        },
+                    }
+                }
+            ]
         }]
     },
     mode: 'development',
